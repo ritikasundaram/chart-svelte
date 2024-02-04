a11ec0545249f3f277606d9c9e4a8ea79cb9999b
manuvikash <ritikasundaram2002@gmail.com>
2024-02-05 00:47:26 +0530

    Works for me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-
+<!--This is the main home page that redirects to every other page -->
+  
   <div>
 	<a href = "/carrierwise"><h1>Carrierwise returns</h1></a>
 	<a href = "/payment"><h1>payment returns</h1></a>
